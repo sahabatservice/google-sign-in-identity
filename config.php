8fd2f240ea45ba42a352e4f0e8c84cd940653a69
Yazarake <30499177+sahabatservice@users.noreply.github.com>
2019-01-03 13:01:26 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require ("vendor/autoload.php");
+require_once "vendor/autoload.php";
 
 $redirect_uri       = "<Your REDIRECT URI>";
 $client_id          = "<Your Client ID>";
